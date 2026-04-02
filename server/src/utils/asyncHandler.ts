3595479f5cc44537214bc289db64733a3d35140f
Mustafa Sayyed <mustafasayyed2429@gmail.com>
2026-04-02 23:01:39 +0530

    add helper functions

---
+++ server/src/utils/asyncHandler.ts
@@ -0,0 +1,9 @@
+import type { Request, Response, NextFunction } from "express";
+
+function asyncHandler(fn: Function) {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+}
+
+export default asyncHandler;
---
+++ server/src/utils/httpStatusCodes.ts
@@ -0,0 +1,11 @@
+const httpStatusCodes = {
+  OK: 200,
+  CREATED: 201,
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  NOT_FOUND: 404,
+  TOO_MANY_REQUEST: 429,
+  INTERNAL_SERVER_ERROR: 500,
+};
+
+export default httpStatusCodes;
